f75686c962e01b8fbdcd34d2d92faef205199558
Valeriy Kokorev <kokorev.valery@gmail.com>
2018-09-19 00:44:57 +0700

    Добавил анимацию при ошибке заполнения контактной формы

--- js/script.js
+++ js/script.js
@@ -1,21 +1,58 @@
 var contactsLink = document.querySelector(".contacts__button");
 var contactsModal = document.querySelector(".modal-contacts");
 var contactsClose = contactsModal.querySelector(".modal__close");
+
+var contactsForm = contactsModal.querySelector(".contacts-form");
 var contactsName = contactsModal.querySelector("[name='name']");
+var contactsEmail = contactsModal.querySelector("[name='email']");
+var contactsMessage = contactsModal.querySelector("[name='message']");
 
 var mapLink = document.querySelector(".contacts__map-link");
 var mapModal = document.querySelector(".modal-map");
 var mapClose = mapModal.querySelector(".modal__close");
 
+var isStorageSupport = true;
+var nameStorage = "";
+var emailStorage = "";
+
+try {
+  nameStorage = localStorage.getItem("name");
+  emailStorage = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 contactsLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsModal.classList.add("modal--show");
-  contactsName.focus();
+
+  if (nameStorage || emailStorage) {
+    contactsName.value = nameStorage ? nameStorage : "";
+    contactsEmail.value = emailStorage ? emailStorage : "";
+    contactsMessage.focus();
+  } else {
+    contactsName.focus();
+  }
 });
 
 contactsClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsModal.classList.remove("modal--show");
+  contactsModal.classList.remove("modal--error");
+});
+
+contactsForm.addEventListener("submit", function (evt) {
+  if (!contactsName.value || !contactsEmail.value || !contactsMessage.value) {
+    evt.preventDefault();
+    contactsModal.classList.remove("modal--error");
+    contactsModal.offsetWidth = contactsModal.offsetWidth;
+    contactsModal.classList.add("modal--error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", contactsName.value);
+      localStorage.setItem("email", contactsEmail.value);
+    }
+  }
 });
 
 mapLink.addEventListener("click", function (evt) {
@@ -34,6 +71,7 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (contactsModal.classList.contains("modal--show")) {
       contactsModal.classList.remove("modal--show");
+      contactsModal.classList.remove("modal--error");
     }
     if (mapModal.classList.contains("modal--show")) {
       mapModal.classList.remove("modal--show");
